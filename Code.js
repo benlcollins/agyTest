e830908d7e97d1c47e3bbe4f1bcaaf5095581745
benlcollins <ben@benlcollins.com>
2026-02-17 12:28:40 -0500

    added logHello function via Gemini

--- Code.js
+++ Code.js
@@ -1,3 +1,11 @@
-function myFunction() {
-  console.log('Hello world!');
+function agyTest() {
+  var ui = SpreadsheetApp.getUi();
+  var response = ui.prompt('Please enter your name:');
+
+  if (response.getSelectedButton() == ui.Button.OK) {
+    var name = response.getResponseText();
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+    var cell = sheet.getActiveCell();
+    cell.setValue('Hello ' + name);
+  }
 }
